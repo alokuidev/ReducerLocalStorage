1f75b120334cbaad1fee34d3fd561a755dcdb6c5
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-02-17 23:49:45 +0530

    base commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+
+  return (
+    <>
+      <div className="todo-container">
+        <h1>To-Do List</h1>
+        <div className="todo-input">
+            <input type="text" id="taskInput" placeholder="Enter a new task..."/>
+            <button>Add Task</button>
+        </div>
+        <ul id="taskList">
+            <li className="todo-item">
+                <input type="checkbox"/>
+                <span className="task-text">Learn Redux</span>
+                <button className="remove-btn">X</button>
+            </li>
+            <li className="todo-item">
+                <input type="checkbox"/>
+                <span className="task-text">Complete React Project</span>
+                <button className="remove-btn">X</button>
+            </li>
+        </ul>
+    </div>
+    </>
+  )
+}
+
+export default App

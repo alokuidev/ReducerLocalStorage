e9dad7f6d0ade06e188c42ff56239a0562a64192
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-02-26 18:29:43 +0530

    optimized

--- src/context/Todoprovider.jsx
+++ src/context/Todoprovider.jsx
@@ -3,31 +3,43 @@ import ToDoContext from "./todoContext";
 
 function Todoprovider({ children }) {
   const [todoList, setTodoList] = useState([]);
-  
+
   //adding value to todoList
   const addTodo = (todo) => {
     const newTodo = {
-        id:Date.now(),
-        todo:todo,
-        completed:false
-    } 
-    setTodoList((prev)=>[...prev,newTodo])
-   };
-   const updateTodo = (id, todos) => { 
-    setTodoList((prev) => 
-      prev.map((elem) => 
-        elem.id === id ? { ...elem, todo: todos } : elem // Add `: elem` to return unchanged items
+      id: Date.now(),
+      todo: todo,
+      completed: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+  };
+
+
+  const updateTodo = (id, todos) => {
+    setTodoList((prev) =>
+      prev.map(
+        (elem) => (elem.id === id ? { ...elem, todo: todos } : elem) // Add `: elem` to return unchanged items
       )
     );
-};
-  
+  };
+
   //deleted value
   const deleteTodo = (id) => {
-    setTodoList((prev) => prev.filter((elem) => elem.id !== id ));
+    setTodoList((prev) => prev.filter((elem) => elem.id !== id));
   };
   const toggleComplete = (id) => {};
+  
   return (
-    <ToDoContext.Provider value={{ todoList, setTodoList, addTodo,updateTodo, deleteTodo,toggleComplete }}>
+    <ToDoContext.Provider
+      value={{
+        todoList,
+        setTodoList,
+        addTodo,
+        updateTodo,
+        deleteTodo,
+        toggleComplete,
+      }}
+    >
       {children}
     </ToDoContext.Provider>
   );
